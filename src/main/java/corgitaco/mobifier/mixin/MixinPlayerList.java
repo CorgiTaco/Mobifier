222c029fd89b4f11a1a2258d78b88c4978595043
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-12-27 17:52:09 -0800

    Actually sync config on client

--- src/main/java/corgitaco/mobifier/mixin/MixinPlayerList.java
+++ src/main/java/corgitaco/mobifier/mixin/MixinPlayerList.java
@@ -1,5 +1,8 @@
 package corgitaco.mobifier.mixin;
 
+import corgitaco.mobifier.common.MobifierConfig;
+import corgitaco.mobifier.common.network.NetworkHandler;
+import corgitaco.mobifier.common.network.packet.MobifierConfigSyncPacket;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.management.PlayerList;
 import net.minecraft.world.server.ServerWorld;
@@ -13,5 +16,6 @@ public class MixinPlayerList {
 
     @Inject(method = "sendLevelInfo", at = @At(value = "HEAD"))
     private void sendContext(ServerPlayerEntity playerIn, ServerWorld worldIn, CallbackInfo ci) {
+        NetworkHandler.sendToPlayer(playerIn, new MobifierConfigSyncPacket(MobifierConfig.getConfig()));
     }
 }
