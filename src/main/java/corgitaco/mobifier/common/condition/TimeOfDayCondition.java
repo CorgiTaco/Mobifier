ff301f072cd1708df42cc04872f4e585ed82fade
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2022-05-04 09:30:54 -0700

    Fix spelling mistake in times of day codec key.

--- src/main/java/corgitaco/mobifier/common/condition/TimeOfDayCondition.java
+++ src/main/java/corgitaco/mobifier/common/condition/TimeOfDayCondition.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class TimeOfDayCondition implements Condition {
 
     public static final Codec<TimeOfDayCondition> CODEC = RecordCodecBuilder.create(builder -> {
-        return builder.group(LongPair.createLongPairCodec("min_time", "max_ime").listOf().fieldOf("times_of_day").forGetter(timeOfDayCondition -> timeOfDayCondition.timesOfDay),
+        return builder.group(LongPair.createLongPairCodec("min_time", "max_time").listOf().fieldOf("times_of_day").forGetter(timeOfDayCondition -> timeOfDayCondition.timesOfDay),
                 Codec.LONG.optionalFieldOf("day_length", 24000L).forGetter(timeOfDayCondition -> timeOfDayCondition.dayLength)
         ).apply(builder, TimeOfDayCondition::new);
     });
